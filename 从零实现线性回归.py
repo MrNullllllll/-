8bb8da457ce72b2560810d913cf815b47e521bf9
Null <101175572+MrNullllllll@users.noreply.github.com>
2022-03-28 19:43:47 +0800

    Update 从零实现线性回归.py

--- 从零实现线性回归.py
+++ 从零实现线性回归.py
@@ -5,7 +5,7 @@ from d2l import torch as d2l
 
 def synthetic_data(w,b,num_examples):
     X = torch.normal(0, 1, (num_examples, len(w)))
-    w = w.reshape(-1,1)
+    w=w.reshape(-1,1)
     Y = torch.matmul(X, w) + b
     Y += torch.normal(0, 0.01, Y.shape)
     return X, Y
@@ -29,9 +29,11 @@ def squared_loss(y_hat, y):  #@save
 
 def sgd(params, lr):  #@save
     """小批量随机梯度下降"""
-    for param in params:
-        param -= lr * param.grad
-        param.grad.zero_()
+    # 尚未理解with torch.no_grad()的含义，暂时搁置
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad
+            param.grad.zero_()
 
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
@@ -51,11 +53,12 @@ for epoch in range(num_epochs):
     for X, Y in data_iter(batch_size, features, labels):
         l = loss(net(X, w, b), Y)  # X和y的小批量损失
         # 因为l形状是(batch_size,1)，而不是一个标量。l中的所有元素被加到一起，
-        l.mean().backward()
         # 并以此计算关于[w,b]的梯度
+        l.mean().backward()
         sgd([w, b], lr)  # 使用参数的梯度更新参数
-    train_l = loss(net(features, w, b), labels)
-    print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+    with torch.no_grad():
+        train_l = loss(net(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
 
 print(f'w的估计误差: {true_w - w.reshape(true_w.shape)}')
 print(f'b的估计误差: {true_b - b}')
